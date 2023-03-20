ec2a6b13a03d62a73b6a467ec8c44c1ef5270fbe
PCGAMER-DIANA\Diana <diana.fp.gomes@gmail.com>
2023-03-20 17:42:11 +0000

    Add test from PriceStatistics component

---
+++ src/__tests__/components/coin/PriceStatistics.test.tsx
@@ -0,0 +1,30 @@
+import { PriceStatistics } from '@components/coin/PriceStatistics';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { statistics } from '@mocks/coins';
+
+describe('PriceStatistics', () => {
+  it('Test data', () => {
+    render(<PriceStatistics coinName={'Bitcoin'} statistics={statistics} />);
+
+    expect(screen.getAllByRole('row')).toHaveLength(10);
+
+    expect(screen.getByText('Price')).toBeInTheDocument();
+    expect(screen.getByText('24h Low / 24h High')).toBeInTheDocument();
+    expect(screen.getByText('7d Low / 7d High')).toBeInTheDocument();
+    expect(screen.getByText('Trading Volume')).toBeInTheDocument();
+    expect(screen.getByText('Market Cap Rank')).toBeInTheDocument();
+    expect(screen.getByText('Market Cap')).toBeInTheDocument();
+    expect(screen.getByText('Market Cap Dominance')).toBeInTheDocument();
+    expect(screen.getByText('Volume / Market Cap')).toBeInTheDocument();
+    expect(screen.getByText('All-Time High')).toBeInTheDocument();
+    expect(screen.getByText('All-Time Low')).toBeInTheDocument();
+
+    expect(screen.getByText('$26,674.00')).toBeInTheDocument();
+    expect(screen.getByText('$57,009,919,159.00')).toBeInTheDocument();
+    expect(screen.getByText('#1')).toBeInTheDocument();
+    expect(screen.getByText('$517,011,186,274.00')).toBeInTheDocument();
+    expect(screen.getByText('0.0%')).toBeInTheDocument();
+    expect(screen.getByText('$0.11')).toBeInTheDocument();
+  });
+});
